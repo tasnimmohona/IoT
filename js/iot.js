97c057a00f4b891100aeab5db8d0124a4bf102f9
Tasnim Mohona <67105895+tasnimmohona62@users.noreply.github.com>
2020-08-20 22:59:54 +0600

    afterlogin room+sidebar

--- js/iot.js
+++ js/iot.js
@@ -133,3 +133,34 @@ $(window).on("load", function() {
     $(".preloader").fadeOut();
 });
 
+//
+$(".sidebar-dropdown > a").click(function() {
+    $(".sidebar-submenu").slideUp(200);
+    if (
+      $(this)
+        .parent()
+        .hasClass("active")
+    ) {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .parent()
+        .removeClass("active");
+    } else {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .next(".sidebar-submenu")
+        .slideDown(200);
+      $(this)
+        .parent()
+        .addClass("active");
+    }
+  });
+  
+  $("#close-sidebar").click(function() {
+    $(".page-wrapper").removeClass("toggled");
+  });
+  $("#show-sidebar").click(function() {
+    $(".page-wrapper").addClass("toggled");
+  });
+  
+  
